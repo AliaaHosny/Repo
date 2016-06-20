1bfdb7a6d937e83a7774a4bb5abccda18f92ba0c
AliaaHosny <aliaahosny77@gmail.com>
2016-06-20 04:37:09 -0700

    Adding pages

---
+++ js/my-js.js
@@ -0,0 +1,67 @@
+$(document).ready(function() {
+	$(".repo1").mouseenter(function(){
+		$(".form").fadeToggle();
+		
+		}); 
+	$(".repo1").mouseleave(function(){
+		$(".form").fadeToggle();
+		
+		}); 
+
+$(".repo2").mouseenter(function(){
+		$(".form2").fadeToggle();
+		
+		}); 
+	$(".repo2").mouseleave(function(){
+		$(".form2").fadeToggle();
+		
+		}); 
+
+	$(".repo3").mouseenter(function(){
+		$(".form3").fadeToggle();
+		
+		}); 
+	$(".repo3").mouseleave(function(){
+		$(".form3").fadeToggle();
+		
+		}); 
+	$(".repo4").mouseenter(function(){
+		$(".form4").fadeToggle();
+		
+		}); 
+	$(".repo4").mouseleave(function(){
+		$(".form4").fadeToggle();
+		
+		}); 
+
+$(".header-1").mouseenter(function(){
+		$(".form-1").fadeToggle();
+		
+		}); 
+	$(".header-1").mouseleave(function(){
+		$(".form-1").fadeToggle();
+		
+		});
+
+$(".header-2").mouseenter(function(){
+		$(".form-2").fadeToggle();
+		
+		}); 
+	$(".header-2").mouseleave(function(){
+		$(".form-2").fadeToggle();
+		
+		});
+	$(".header-3").mouseenter(function(){
+		$(".form-3").fadeToggle();
+		
+		}); 
+	$(".header-3").mouseleave(function(){
+		$(".form-3").fadeToggle();
+		
+		});
+});
+$(document).ready(function(){
+    $('[data-toggle="popover"]').popover();   
+});
+	
+
